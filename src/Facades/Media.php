79fa38c47e05714ba2811ebb85a5a7ab8f15c827
MD. Anichur Rahaman <38912435+anisAronno@users.noreply.github.com>
2023-08-29 00:08:57 +0600

    Dynamic Default File Get Method

--- src/Facades/Media.php
+++ src/Facades/Media.php
@@ -3,20 +3,49 @@
 namespace AnisAronno\MediaHelper\Facades;
 
 use Illuminate\Support\Facades\Facade;
+use Illuminate\Support\Str;
 
 class Media extends Facade
 {
     /**
-      * @method static void \AnisAronno\MediaHelper\MediaHelpers setStorageDisk(string $disk)
-      * @method static string \AnisAronno\MediaHelper\MediaHelpers getDefaultFiles(true)
-       * @method static string \AnisAronno\MediaHelper\MediaHelpers getURL(string $path)
-      * @method static void \AnisAronno\MediaHelper\MediaHelpers upload($request, $fieldName, string $upload_dir)
-      * @method static void \AnisAronno\MediaHelper\MediaHelpers public function delete($value)
+      * @method static void setStorageDisk(string $disk)
+      * @method static string getDefaultFiles(bool $includePath = false)
+      * @method static string getDefaultLogo()
+      * @method static string getDefaultFavIcon()
+      * @method static string getDefaultBanner()
+      * @method static string getDefaultAvatar()
+      * @method static string getDefaultPlaceholder()
+      * @method static string getURL(string $path)
+      * @method static void upload($request, $fieldName, string $upload_dir)
+      * @method static void delete($value)
       *
-      * @see \AnisAronno\MediaHelper\MediaHelpers
+      * @see \AnisAronno\MediaHelper\Media
       */
     protected static function getFacadeAccessor()
     {
         return 'Media';
     }
+
+    public static function __callStatic($method, $arguments)
+    {
+        $instance = static::resolveFacadeInstance(static::getFacadeAccessor());
+
+        if(Str::startsWith($method, 'getDefault')) {
+            $files = $instance->getAllDefaultFiles(true);
+            $key = strtolower(trim(str_replace('getDefault', '', $method)));
+
+            if (array_key_exists($key, $files)) {
+                return $files[$key];
+            } else {
+                throw new \BadMethodCallException("Method {$method} does not exist.");
+            }
+        } else {
+            try {
+                return call_user_func_array([$instance, $method], $arguments);
+            } catch (\Throwable $th) {
+                throw new \BadMethodCallException($th->getMessage());
+            }
+        }
+    }
+
 }
